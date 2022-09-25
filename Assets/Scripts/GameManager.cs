a2a537f7effd7fc5f097d9ceec9bcc3732ed000e
Madhava R <ravee006@umn.edu>
2022-09-24 22:28:00 -0500

    Spawning Initiated

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Puff;
+    public GameObject Node;
+    public GameObject Obstacle;
+
+    // Ranges
+    private Vector2 xPosRange = new Vector2(-10.5f,10.5f);
+    private Vector2 yPosRange = new Vector2(-4.5f,4.5f);
+
+    // Quantities
+    private int numObstacles = 10;
+    private int numNodes = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numObstacles = Random.Range(20, 25);
+        numNodes = Random.Range(15, 20);
+
+        // Generate Sprites
+        for (int i = 0; i < numObstacles; i++)
+        {
+            Vector2 obstaclePos = new Vector2(Random.Range(xPosRange.x, xPosRange.y), Random.Range(yPosRange.x, yPosRange.y));
+            Instantiate(Obstacle,obstaclePos, Quaternion.identity);
+        }
+
+        for (int i = 0; i < numNodes; i++)
+        {
+            Vector2 nodePos = new Vector2(Random.Range(xPosRange.x, xPosRange.y),Random.Range(yPosRange.x,yPosRange.y));
+            Instantiate(Node,nodePos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
